8545463337ef00afca3920c8d43aa0440c6ee077
AmirHome <amir.email@yahoo.com>
2018-06-05 12:34:13 +0300

    Menu Sidebar

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,16 @@
+<div class="page-sidebar-wrapper">
+    <div class="page-sidebar navbar-collapse collapse">
+        <ul class="page-sidebar-menu"
+            data-keep-expanded="false"
+            data-auto-scroll="true"
+            data-slide-speed="200">
+            <li class="active">
+                <a href="{{ url('/persons') }}">
+                    <i class="fa fa-users"></i>
+                    <span class="title">Persons</span>
+                </a>
+            </li>
+            
+        </ul>
+    </div>
+</div>
