ec53344e76c8e55208731239737034375347d01c
AmirHome <amir.email@yahoo.com>
2018-06-05 16:56:48 +0300

    test for path css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <link href="{{url('/resources/assets/css/amirhome_style.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{url('./resources/assets/css/amirhome_style.css')}}" rel="stylesheet" type="text/css">
 
     </head>
     <body>
